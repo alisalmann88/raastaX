ba56b9670d2129f20811662ba044efc4077d17d2
Ali <aleesalman99.com>
2026-01-17 00:10:36 +0500

    Use Railway environment variables for DB

--- db.js
+++ db.js
@@ -1,16 +1,10 @@
-// db.js
-const mysql = require("mysql2/promise"); // make sure mysql2 is installed
+const mysql = require("mysql");
 
-const pool = mysql.createPool({
-  host: "localhost",     // your MySQL host
-  user: "root",          // your MySQL username
-  password: ";5LZmgC3+zu*5K", // your MySQL password
-  database: "raastax_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-module.exports = pool;
-
-
+module.exports = con;
