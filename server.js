313ddee9d773c5c77db1f60a9c0657541a70d5d3
Ali Salman <157398464+alisalmann88@users.noreply.github.com>
2026-01-17 19:46:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -100,6 +100,8 @@ app.use((req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
+});
+
 });
