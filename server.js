694621eeea0893edc524ab62303c831da1764c13
Ali Salman <157398464+alisalmann88@users.noreply.github.com>
2026-01-17 21:52:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', (req, res) => {
 });
 
 // Use PORT 8080 as your logs show
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`
   ==================================
